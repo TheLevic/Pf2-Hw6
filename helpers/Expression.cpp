2fc0c81c5dbae62420f8faded806eebaeb07efaa
TheLevic <levicrider08@gmail.com>
2021-12-01 10:12:44 -0600

    Fixed Expression and Token Class

--- helpers/Expression.cpp
+++ helpers/Expression.cpp
@@ -2,111 +2,111 @@
 #include "Token.h"
 
 
-//For ease of use in my methods I created this
-bool isSpecial(const string& s, int i){
-    if(s[i] == '(' || s[i] == ')' || s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '='){
-        return true;
-    }
-    else{
-        return false;
-    }
+Expression::Expression()
+{
+	original = "";
+	valid = false;
+	type = Bad;
 }
 
-
-
-Expression::Expression(){
-    original = ""; //Making sure original is Empty
-    type = Bad; //Setting type to bad
-    valid = false; //Setting valid to be false initially
-    tokenized.clear(); //Making sure the inital vector is empty
-    postfix.clear(); //Making sure the inital vector is empty
+Expression::Expression(const string& s)
+{
+	set(s);
 }
 
+void Expression::set(const string& s)
+{
+	original = s;
+	tokenized.clear();
+	string track = "";
+	string space = " ";
+	for(int i = 0; i < s.length(); i++)
+	{
+		if(s[i] == ' '){
+			if(!track.empty())
+			{
+				tokenized.push_back(Token(track));
+				track = "";
+			}
+		}
+		else if((s[i] == '+') || (s[i] == '-') || (s[i] == '*') || (s[i] == '/') || (s[i] == '(') || (s[i] == ')') || (s[i] == '=')){
+			if(!track.empty()) {
+				tokenized.push_back(Token(track));
+				track = "";
+			}
+			track += s[i];
+			tokenized.push_back(Token(track));
+			track = "";
+		}
+		else
+		{
+			track += s[i];
+		}
 
-Expression::Expression(const string& s){
-    set(s); //Using the set method
+	}
+	if(track != "")
+		tokenized.push_back(Token(track));
+
+
+// 	for(int i=0; i<s.length(); i++)
+// 	{
+// 		//if s[i] is empty " " s[i] == " "
+// 			// do something with token you had earlier tokenized.push_back(token)
+//			//if track was empty, continue;
+// 			// track = empty, track = "";
+// 		//if special token (s[i] == "+" || - || * || / || ( || ) || =)
+// 			// stop the  track variable you had earlier and add that to vector
+// 			// add the operator, (), or = to vector
+// 		 	// set track empty
+// 		//else
+// 			// letter or number, other
+// 			// += add that stuff to the track variable 
+// 		tokenized.push_back(track variable);
+
+	valid = false;
+	type = Bad;
 }
 
-
-void Expression::set(const string& s){
-    //We need to separate each character based on the definition of a token (tokens are separated by spaces or special characters);
-    //Once the first token is found, add it into tokenized
-    //After adding into tokenized, repeat this process from the next position
-    //at which we stopped at until finding the terminating NULL character (hence the do while loop)
-
-    string semi = ";";
-    original = s; //Setting original
-    string tmp; //Temporary string to hold values
-    tmp.clear(); //Making sure tmp is clear.
-    tokenized.clear(); //Making sure the vector is clear
-    int i = 0; //Keeping our place
-    do
-    {
-        
-        if (isSpecial(s,i) == false && s[i] != ' ' && s[i] != '\0'){ //If it's an ordinary character
-            tmp =  tmp + s[i]; //Setting tmp = to itself + the next char
-            i++; //moving on
-        }
-
-        if (isSpecial(s,i) == true){
-            if (tmp.empty() != true){ //If tmp is not empty
-                tokenized.push_back(tmp); //Adding tmp to vector
-                tmp.clear(); //Clear tmp
-                tmp = s[i]; //Setting tmp = to our char
-                tokenized.push_back(tmp); //Adding tmp to our vector
-                tmp.clear(); //Clearing tmp
-                i++; //Moving on
-            }
-            else if(tmp.empty() == true){ // if Tmp is empty
-                tmp = s[i]; //Setting tmp to our char
-                tokenized.push_back(tmp); //Adding to vector
-                tmp.clear(); //Clearing tmp
-                i++; //Moving on
-            }
-        }
-
-        if(s[i] == ' '){ //If s is equal to space
-            if (tmp.empty() != true){ //And if tmp is not empty
-                tokenized.push_back(tmp); //Adding tmp to our vector
-                tmp.clear(); //clearing tmp
-                i++; //Moving on
-            }
-            else{ //If it is empty
-                i++; //Move on
-            }
-        }
-        if(s[i] == '\0'){ //Once we reach the NULL char
-            if (tmp.empty() != true){ //If tmp is not empty
-                tokenized.push_back(tmp); //Adding tmp to vector
-                tmp.clear(); //Clearing tmp
-                break;//Exiting loop
-            } 
-            else{
-                break; //Exiting loop if tmp is empty
-            }
-        }
-    } while (s[i] != '\0');
+void Expression::display() const
+{
+	cout << "original = " << original << endl;
+	cout << "tokenized = ";
+	for(int i = 0; i < tokenized.size(); i++) {
+		cout << tokenized.at(i).get_token() << ";";
+	}
+	cout << endl;
+	cout << "number of tokens = " << tokenized.size() << endl;
+	cout << "postfix = " << endl;
+	string validName;
+	if(valid==false)
+		validName = "false";
+	else
+		validName = "true";
+	cout << "valid = " << validName << endl;
+	string nameType;
+	switch(type) {
+		case 0:
+			nameType = "Assignment";
+			break;
+		case 1: 
+			nameType = "Arithmetic";
+			break;
+		case 2:
+			nameType = "Bad";
+			break;
+		default:
+			nameType = "Invalid";
+	}
+	cout << "type = " << nameType << endl;
 }
 
-
-
-void Expression::display() const{
-    string semi = ";";
-    cout << "original = " << original << endl;
-    cout << "tokenized = ";
-    for (int i = 0; i < tokenized.size(); i++){
-       cout << tokenized.at(i).get_token() + semi;
-    }
-    cout << endl;
-    cout << "postfix = " << endl;
-    cout << "valid = " << valid << endl;
-    cout << "type = " << type << endl;
-    cout << "----------------------------------------" << endl;
+string Expression::getoriginal() const
+{
+	return original;
 }
 
-string Expression::getoriginal() const{
-    return original;
+vector<Token> Expression::get_tokenized() const
+{
+	return tokenized;
 }
-vector<Token> Expression::get_tokenized() const{
-    return tokenized;
-}
+
--- helpers/Token.cpp
+++ helpers/Token.cpp
@@ -1,144 +1,160 @@
 #include "Token.h"
-//Method to use in our constructor and setter
-void Token::checkString(string s){
-    //Check if it's empty
-    if (s.empty()){
-        type = INVALID;
-        priority = -1;
-    }
 
+Token::Token()
+{
+	type = INVALID;
+	token = "";
+	priority = -1;
+}
 
-    //Check if s is an identifier (string that starts with a letter)
-    if (s[0] != '\0' && isalpha(s[0])){ //I think I need to check each letter to make sure that there is not a special char
-        type = Identifier;
-        priority = -1;
-    }
-    
-    //Check if s is an integer (Make sure it doesn't start with 0)
-    else if(isdigit(s[0])){
-        bool number = true;
-        for(int i = 0; i < s.length(); i++){
-            if (s[i] == '0' && s.length() > 1){
-                number = false;
-                break;
-            }
-            if(isdigit(s[i])){
-                continue;
-            }
-            else{
-                number = false;
-                break;
-            }
-        }
-        if (number){
-            type = Integer;
-        }
-        else{
-            type = INVALID;
-        }
-        priority = -1;
-    }
+Token::Token(string s)
+{
+	set(s);
+}
 
-    //check if s is an operator
-    else if (s == "+" || s == "-" || s == "*" || s =="/"){
-        type = Operators;
-        if (s == "+" || s == "-"){
-            priority = 1;
-        }
-        else{
-            priority = 2;
-        }
-    }
-    //check if s is an equal sign
-    else if (s == "="){
-        type = EqualSign;
-        priority = -1;
-    }
-    //check if s is open parentheses
-    else if(s == "("){
-        type = OpenBrace;
-        priority = 0;
-    }
-    //check if s is close parantheses
-    else if(s == ")"){
-        type = CloseBrace;
-        priority = -1;
-    }
-    //anything else
-    else{
-        priority = -1;
-        type = INVALID;
-    }
+void Token::set(string s)
+{
+	// takes in a string and treats it as a token
+	token = s;
+	
+	// s is empty
+	if(s.empty()){
+		type = INVALID;
+		priority = -1;
+	}
+	// check if s is an identifier
+	else if(isalpha(s[0])){
+		bool ident = true;
+		for(int i = 1; i < s.length(); i++){
+			if(isalnum(s[i]))
+				continue;
+			else {
+				ident = false;
+				break;
+			}
+		}
+		if(ident)
+			type = Identifier;
+		else
+			type = INVALID;
+		priority = -1;
+	}
+	// check if s an integer
+	else if(isdigit(s[0])) {
+		if(s.length() == 1) {
+			type = Integer;
+			priority = -1;
+		}
+		else if((s.length() > 1) && (s[0] != '0')) {
+			bool number = true;
+			for (int i = 0; i < s.length(); i++){
+				if(isdigit(s[i]))
+					continue;
+				else {
+					number = false;
+					break;
+				}
+			}
+			if(number)
+				type = Integer;
+			else
+				type = INVALID;
+			priority = -1;
+		}
+		else
+			return;
+	}
+
+	// check if s is an operator
+	else if( (s == "+") || (s == "-") || (s == "*") || (s == "/")) {
+		type = Operators;
+		if(s == "+" || s == "-")
+			priority = 1;
+		else
+			priority = 2;
+	}
+	// check if s is =
+	else if(s == "="){
+		type = EqualSign;
+		priority = -1;
+	}
+	// check if s is openbrace
+	else if(s == "("){
+		type = OpenBrace;
+		priority = 0;
+	}
+	// check if s is closebrace
+	else if(s == ")"){
+		type = CloseBrace;
+		priority = -1;
+	}
+	//all else is INVALID
+	else{
+		priority = -1;
+		type = INVALID;
+	}
+
+}
+
+int Token::value() const
+{
+	if(type == Integer){
+		return stoi(token);
+	}
+	else if(type == Identifier)
+		return -1;
+	else
+		return -2;
+}
+
+void Token::display() const
+{
+	string typeName;
+	switch(type){
+		case 0:
+			typeName = "Identifier";
+			break;
+		case 1:
+			typeName = "Integer";
+			break;
+		case 2:
+			typeName = "Operators";
+			break;
+		case 3:
+			typeName = "EqualSign";
+			break;
+		case 4:
+			typeName = "OpenBrace";
+			break;
+		case 5:
+			typeName = "CloseBrace";
+			break;
+		case 6:
+			typeName = "INVALID";
+			break;
+		default:
+			typeName = "nothing";
+	}
+	cout << "type = " << typeName << endl;
+	cout << "token = " << token << " (value is " << value() << ")" << endl;
+	cout << "priority = " << priority << endl;
+
+}
+
+Token_type Token::get_type() const
+{
+	return type;
+}
+
+string Token::get_token() const
+{
+	return token;
+}
+
+int Token::get_priority() const
+{
+	return priority;
 }
 
 
 
-Token::Token(){
-    type = INVALID;
-    token = "";
-    checkString(token);
-}
-
-Token::Token(string s){
-    token = s;
-    checkString(s);
-}
-
-
-void Token::set(string s){
-    token = s;
-    checkString(s);
-}
-
-int Token::value() const{
-    int number = 0;
-    if(type == Integer){
-        number = stoi(token);
-        return number;
-    }
-    else if(type == Identifier){
-        return -1;
-    }
-    else{
-        return -2;
-    }
-}
-
-void Token::display() const{
-    string nameType = "";
-    switch(type){
-        case 0:
-            nameType = "Identifier";
-            break;
-        case 1:
-            nameType = "Integer";
-            break;
-        case 2:
-            nameType = "Operators";
-            break;
-        case 3:
-            nameType = "EqualSign";
-            break;
-        case 4:
-            nameType = "OpenBrace";
-        case 5:
-            nameType = "CloseBrace";
-        case 6:
-            nameType = "INVALID";
-    }
-    cout << "type = " << nameType << endl;
-    cout << "token = " << token << "(value is " << value() << ")" << endl;
-    cout << "priority = " << priority << endl;
-}
-
-Token_type Token::get_type()const{
-    return type;
-}
-
-string Token::get_token() const{
-    return token;
-}
-
-int Token::get_priority() const{
-    return priority;
-}
